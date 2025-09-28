879e3826725d97dc0935a9a354fdce341c743246
Dennis-Mugo <dennismugo2003@gmail.com>
2025-09-28 09:36:11 +0300

    removed duplicate cred

--- firebase_config/config.py
+++ firebase_config/config.py
@@ -3,7 +3,6 @@ from firebase_admin import credentials, storage, firestore, auth
 
 # cred = credentials.Certificate("firebase_config/private.json")
 cred = credentials.Certificate("private.json")
-cred = credentials.Certificate("private.json")
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, {'storageBucket': 'sheriai.appspot.com'})
 
